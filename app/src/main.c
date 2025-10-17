0a5281c5c0b8e8a404e2f27373b5b5c3cf940301
bihanw2 <bihanw2@gmail.com>
2025-10-16 18:57:45 -0600

    initial button setup

--- app/src/main.c
+++ app/src/main.c
@@ -8,10 +8,30 @@
 #include <zephyr/sys/printk.h>
 #include <inttypes.h>
 
+#define SLEEP_TIME_MS 1000
+#define SW0_NODE DT_ALIAS(sw0)
+
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
+
 int main(void) {
+    
+    int ret;
 
-  while(1) {
+    if (!gpio_is_ready_dt(&button)) {
+        return 0;
+    }
 
-  }
-	return 0;
+    ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+    if (ret < 0) {
+    	return 0;
+    }
+
+    while(1) {
+	ret = gpio_pin_get_dt(&button);
+	if (ret > 0) {
+	    printk("Pressed!\n");
+	}
+	k_msleep(SLEEP_TIME_MS);
+    }
+    return 0;
 }
