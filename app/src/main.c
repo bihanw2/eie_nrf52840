103626edd5b77d0e5c4f1eda37a6c74ce190436d
bihanw2 <bihanw2@gmail.com>
2025-10-16 19:51:46 -0600

    switched to EiE API usage for button/LED init

--- app/src/main.c
+++ app/src/main.c
@@ -11,40 +11,25 @@
 #include "BTN.h"
 #include "LED.h"
 
-#define SLEEP_TIME_MS 1000
-#define SW0_NODE DT_ALIAS(sw0)
-
-static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(SW0_NODE, gpios);
-
-static struct gpio_callback button_isr_data;
-
-void button_isr(const struct device *dev, struct gpio_callback *cb, uint32_t pins) {
-    printk("Button 0 pressed!\n");
-}
+#define SLEEP_TIME_MS 1
 
 int main(void) {
     
-    int ret;
-
-    if (!gpio_is_ready_dt(&button)) {
-        return 0;
-    }
-
-    ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
-    if (ret < 0) {
-    	return 0;
-    }
-
-    ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
-    if (ret < 0) {
+    if (BTN_init() < 0) {
 	return 0;
     }
 
-    gpio_init_callback(&button_isr_data, button_isr, BIT(button.pin));
-    gpio_add_callback(button.port, &button_isr_data);
+    if (LED_init() < 0) {
+	return 0;
+    }
 
     while(1) {
-
+	if (BTN_check_clear_pressed(BTN0)) {
+	    LED_toggle(LED0);
+	    printk("Button 0 pressed!\n");
+	}
+	k_msleep(SLEEP_TIME_MS);
     }
+
     return 0;
 }
