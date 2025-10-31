fd0a1bf668a5ede26f46d733c2b33e89e1605784
bihanw2 <bihanw2@gmail.com>
2025-10-30 18:58:35 -0600

    created state machine source code skeleton

---
+++ app/src/state_machine.c
@@ -0,0 +1,14 @@
+/**
+ * @file state_machine.c
+ */
+
+#include <zephyr/smf.h>
+#include <zephyr/sys/printk.h>
+
+#include "BTN.h"
+#include "LED.h"
+#include "state_machine.h"
+
+void state_machine_init() {}
+
+int state_machine_run() {}
